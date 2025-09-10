b5d9a316ea3c25f81dc148100e78f6273a5ce444
ubterko <ebonkoisrael@gmail.com>
2025-09-10 22:31:33 +0100

    added the radio widget component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,14 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import RadioWidget from '../widgets/RadioWidget';
+import RendererComponent from '../widgets/RendererComponent';
 
-const mockItem = {
-  question: {
-    content: "What is the capital of France?",
-    widgets: {},
-    images: {}
-  },
-  answerArea: {
-    calculator: false,
-    financialCalculatorMonthlyPayment: false,
-    financialCalculatorTimeToPayOff: false,
-    financialCalculatorTotalAmount: false,
-    periodicTable: false,
-    periodicTableWithKey: false
-  }
-};
-
-const mockDependencies = {
-  analytics: {
-    onAnalyticsEvent: () => {}
-  },
-  useVideo: () => ({ result: { video: null } })
-};
 
 function App() {
   return (
     <div>
       <h1>Welcome to Sherlocked!</h1>
-      <RadioWidget item={mockItem} dependencies={mockDependencies} />
+        <RendererComponent />
     </div>
   )
 }
